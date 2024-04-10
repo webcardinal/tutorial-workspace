deb1bdc95ba973c18c0a94af1073fd54ee4e089e
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2024-04-10 17:36:44 +0300

    Implement DSU Explorer installation process
    The code changes introduce the installation process for the DSU Explorer. Changes include updating scripts and links in `package.json`, `index.html` and `configure.json`, adding a new section for 'install-explorer' in `octopus-freeze.json` and creating `environment.js` and `index.html` for DSU Explorer. These updates allow DSU Explorer to be installed via the command `npm install-explorer`.

---
+++ apihub-root/environment.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "vault": "server",
+    "agent": "browser",
+    "system": "any",
+    "browser": "any",
+    "mode": "dev-secure",
+    "vaultDomain": "vault",
+    "didDomain": "vault",
+    "enclaveType": "WalletDBEnclave"
+}
